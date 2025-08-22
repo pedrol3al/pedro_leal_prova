6333edf26372788c1f5272a3c7695c71b0510932
Pedro Gabriel Leal Costa <pedrogabrieleal@gmail.com>
2025-08-22 16:16:01 -0300

    Remoção de permissões
    Remoção das permissões do cliente para acessar a página de consultar produtos

--- principal.php
+++ principal.php
@@ -1,134 +1,30 @@
-<?php
-session_start();
-require_once("conexao.php");
-
-if (!isset($_SESSION['usuario'])) {
-    header("Location: index.php");
-    exit();
-}
-
-//  OBTENDO O NOME DO PERFIL DO USUARIO LOGADO
-
-$id_perfil = $_SESSION['perfil'];
-$sqlPerfil = 'SELECT nome_perfil FROM perfil WHERE id_perfil = :id_perfil';
-$stmtPerfil = $pdo->prepare($sqlPerfil);
-$stmtPerfil->bindParam(':id_perfil', $id_perfil);
-$stmtPerfil->execute();
-$perfil = $stmtPerfil->fetch(PDO::FETCH_ASSOC);
-$nome_perfil = $perfil['nome_perfil'];
-
-//DEFINIÇÃO DAS PERMISSÕES POR PERFIL
-
-$permissoes = [
-    //adm
-    1 => [
-        "Cadastrar" => [
-            "cadastro_usuario.php",
-            "cadastro_perfil.php",
-            "cadastro_cliente.php",
-            "cadastro_fornecedor.php",
-            "cadastro_produto.php",
-            "cadastro_funcionario.php"
-        ],
-        "Buscar" => [
-            "buscar_usuario.php",
-            "buscar_perfil.php",
-            "buscar_cliente.php",
-            "buscar_fornecedor.php",
-            "buscar_produto.php",
-            "buscar_funcionario.php"
-        ],
-        "Alterar" => [
-            "alterar_usuario.php",
-            "alterar_perfil.php",
-            "alterar_cliente.php",
-            "alterar_fornecedor.php",
-            "alterar_produto.php",
-            "alterar_funcionario.php"
-        ],
-        "Excluir" => [
-            "excluir_usuario.php",
-            "excluir_perfil.php",
-            "excluir_cliente.php",
-            "excluir_fornecedor.php",
-            "excluir_produto.php",
-            "excluir_funcionario.php"
-        ]
-    ],
-    //secretaria
-    2 => [
-        "Cadastrar" => ["cadastro_cliente.php"],
-        "Buscar" => ["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php", "buscar_usuario.php"],
-        "Alterar" => ["alterar_fornecedor.php", "alterar_produto.php"],
-        "Excluir" => ["excluir_produto.php"]
-    ],
-    //almoxarife
-    3 => [
-        "Cadastrar" => ["cadastro_fornecedor.php", "cadastro_produto.php"],
-        "Buscar" => ["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
-        "Alterar" => ["alterar_fornecedor.php", "alterar_produto.php"],
-        "Excluir" => ["excluir_produto.php",]
-    ],
-    //cliente
-    4 => [
-        "Cadastrar" => ["cadastro_cliente.php"],
-        "Buscar" => ["buscar_produto.php"],
-        "Alterar" => ["alterar_cliente.php"],
-    ],
-];
-
-//OBTENDO AS OPCOES DISPONIVEIS PARA O PERFIL LOGADO
-$opcoes_menu = $permissoes[$id_perfil];
-
-?>
 
 <!DOCTYPE html>
 <html lang="pt-BR">
-
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Painel principal</title>
     <link rel="stylesheet" href="styles.css">
     <script src="scripts.js"></script>
 </head>
-
 <body>
 
+    <?php include("menu.php"); ?>  
 
-    <nav>
-        <ul class="menu">
-            <?php foreach ($opcoes_menu as $categoria => $arquivos): ?>
-                <li class="dropdown">
-                    <a href="#"><?= $categoria ?></a>
-                    <ul class="dropdown-menu">
-                        <?php foreach ($arquivos as $arquivo): ?>
-                            <li>
-                                <a href="<?= $arquivo ?>"><?= ucfirst(str_replace("_", " ", basename($arquivo, ".php"))) ?></a>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    </nav>
+<br>
+
     <div>
-        <h2>Bem vindo, <?php echo $_SESSION["usuario"]; ?>!<br>Perfil: <?php
-           echo $nome_perfil; ?> </h2>
+        <h2>Bem vindo, <?php echo $_SESSION["usuario"]; ?>!<br>
+        Perfil: <?php echo $nome_perfil; ?> </h2>
     </div>
-
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
     <div class="logout">
         <form action="logout.php" method="POST">
             <button type="submit">Logout</button>
         </form>
-    </div>
-
-
-
+    
+        
     <address>Trabalho desenvolvido pelo aluno Pedro Gabriel | Técnico Dev Sistemas</address>
-
+    </div>
 </body>
-
 </html>
