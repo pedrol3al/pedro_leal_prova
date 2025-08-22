74ff814a742304dae39eb76bf68f7ae8d0bb919d
Pedro Gabriel Leal Costa <pedrogabrieleal@gmail.com>
2025-08-22 16:15:05 -0300

    Arquivo menu
    Esse arquivo armazena o menu dropdawn, para apenas carrega-lo depois em outras paginas

---
+++ menu.php
@@ -0,0 +1,94 @@
+<?php
+// Garante que a sessão está ativa
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+require_once("conexao.php");
+
+// Pega o perfil logado
+$id_perfil = $_SESSION['perfil'];
+
+// Busca nome do perfil
+$sqlPerfil = 'SELECT nome_perfil FROM perfil WHERE id_perfil = :id_perfil';
+$stmtPerfil = $pdo->prepare($sqlPerfil);
+$stmtPerfil->bindParam(':id_perfil', $id_perfil);
+$stmtPerfil->execute();
+$perfil = $stmtPerfil->fetch(PDO::FETCH_ASSOC);
+$nome_perfil = $perfil['nome_perfil'];
+
+// Permissões (mesmo código que você já tem)
+$permissoes = [
+    1 => [
+        "Cadastrar" => [
+            "cadastro_usuario.php",
+            "cadastro_perfil.php",
+            "cadastro_cliente.php",
+            "cadastro_fornecedor.php",
+            "cadastro_produto.php",
+            "cadastro_funcionario.php"
+        ],
+        "Buscar" => [
+            "buscar_usuario.php",
+            "buscar_perfil.php",
+            "buscar_cliente.php",
+            "buscar_fornecedor.php",
+            "buscar_produto.php",
+            "buscar_funcionario.php"
+        ],
+        "Alterar" => [
+            "alterar_usuario.php",
+            "alterar_perfil.php",
+            "alterar_cliente.php",
+            "alterar_fornecedor.php",
+            "alterar_produto.php",
+            "alterar_funcionario.php"
+        ],
+        "Excluir" => [
+            "excluir_usuario.php",
+            "excluir_perfil.php",
+            "excluir_cliente.php",
+            "excluir_fornecedor.php",
+            "excluir_produto.php",
+            "excluir_funcionario.php"
+        ]
+    ],
+    2 => [
+        "Cadastrar" => ["cadastro_cliente.php"],
+        "Buscar" => ["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php", "buscar_usuario.php"],
+        "Alterar" => ["alterar_fornecedor.php", "alterar_produto.php"],
+        "Excluir" => ["excluir_produto.php"]
+    ],
+    3 => [
+        "Cadastrar" => ["cadastro_fornecedor.php", "cadastro_produto.php"],
+        "Buscar" => ["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
+        "Alterar" => ["alterar_fornecedor.php", "alterar_produto.php"],
+        "Excluir" => ["excluir_produto.php"]
+    ],
+    4 => [
+        "Cadastrar" => ["cadastro_cliente.php"],
+        "Alterar" => ["alterar_cliente.php"],
+    ],
+];
+
+// Pega opções do perfil atual
+$opcoes_menu = $permissoes[$id_perfil];
+?>
+<head>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<nav>
+    <ul class="menu">
+        <?php foreach ($opcoes_menu as $categoria => $arquivos): ?>
+            <li class="dropdown">
+                <a href="#"><?= $categoria ?></a>
+                <ul class="dropdown-menu">
+                    <?php foreach ($arquivos as $arquivo): ?>
+                        <li>
+                            <a href="<?= $arquivo ?>"><?= ucfirst(str_replace("_", " ", basename($arquivo, ".php"))) ?></a>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</nav>
