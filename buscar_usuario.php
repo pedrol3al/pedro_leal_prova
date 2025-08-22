15c6697ef3c37eaa6baf844ef9cade7d5f4533eb
Pedro Gabriel Leal Costa <pedrogabrieleal@gmail.com>
2025-08-22 16:11:18 -0300

    Estilização de buscar usuarios
    Estilização da tabela para padronização no sistema

--- buscar_usuario.php
+++ buscar_usuario.php
@@ -34,6 +34,8 @@ $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
+<?php include("menu.php"); ?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 
@@ -42,12 +44,14 @@ $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Busca de funcionarios</title>
     <link rel="stylesheet" href="styles.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 </head>
+
 
 <body>
 
-    <h2>Lista de Usuários</h2>
+    <h2 class="text-center">Lista de Usuários</h2>
 
     <form action="buscar_usuario.php" method="POST">
         <label for="busca">Digite o ID ou NOME(opcional): </label>
@@ -55,9 +59,9 @@ $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <button type="submit">Pesquisar</button>
     </form>
 
-    <?php if(!empty($usuarios)): ?>
+    <?php if (!empty($usuarios)): ?>
 
-        <table class="table">
+        <table class="custom-table">
             <tr>
                 <th>ID</th>
                 <th>Nome</th>
@@ -66,28 +70,35 @@ $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 <th>Ações</th>
             </tr>
 
-        <?php foreach($usuarios as $usuario): ?>
+            <?php foreach ($usuarios as $usuario): ?>
 
-            <tr>
-                <td><?=htmlspecialchars($usuario['id_usuario'])?> </td>
-                <td><?=htmlspecialchars($usuario['nome'])?> </td>
-                <td><?=htmlspecialchars($usuario['email'])?> </td>
-                <td><?=htmlspecialchars($usuario['id_perfil'])?> </td>
-                <td>
-                    <a href="alterar_usuario.php?id=<?=htmlspecialchars($usuario['id_usuario'])?>">Alterar</a>
-                    <a href="excluir_usuario.php?id=<?=htmlspecialchars($usuario['id_usuario'])?>"onclick="return confirm('Tem certeza que você deseja excluir esse usuário?')">Excluir</a>
-                 </td>
-            </tr>
-            <?php endforeach;?>     
+                <tr>
+                    <td><?= htmlspecialchars($usuario['id_usuario']) ?> </td>
+                    <td><?= htmlspecialchars($usuario['nome']) ?> </td>
+                    <td><?= htmlspecialchars($usuario['email']) ?> </td>
+                    <td><?= htmlspecialchars($usuario['id_perfil']) ?> </td>
+                    <td>
+                        <a href="alterar_usuario.php?id=<?= htmlspecialchars($usuario['id_usuario']) ?>">Alterar</a>
+                        <a href="excluir_usuario.php?id=<?= htmlspecialchars($usuario['id_usuario']) ?>"
+                            onclick="return confirm('Tem certeza que você deseja excluir esse usuário?')">Excluir</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
         </table>
-        <?php else:?>
-            <p>Nenhum usuário encontrado.</p>
-        <?php endif;?>   
-        <div class="text-center"> 
-        <a href="principal.php" > Voltar </a>
-        </div>
+    <?php else: ?> 
+        <p>Nenhum usuário encontrado.</p>
+    <?php endif; ?>
 
-        <address class="text-center">Trabalho desenvolvido pelo aluno Pedro Gabriel | Técnico Dev Sistemas</address>
+    <div class="text-center">
+        <a href="principal.php" class="btn-voltar">
+            <button type="button">
+                Voltar
+            </button>
+        </a>
+    </div>
+
+    <address class="text-center">Trabalho desenvolvido pelo aluno Pedro Gabriel | Técnico Dev Sistemas</address>
 
 </body>
+
 </html>
